f61705cb4b9668f5bf66680a2cc97908dcdc6ffc
shouts77 <shouts98@gmail.com>
2025-05-02 15:18:59 +0900

    add top menu and page structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../styles/app.css';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<h1>home</h1>
+<p>this is the home page.</p>
