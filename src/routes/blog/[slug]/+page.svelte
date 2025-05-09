d0b4c690275d2631847ca76f91987349f8991254
shouts77 <shouts98@gmail.com>
2025-05-09 09:00:20 +0900

    add date formatting util

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,10 +1,11 @@
 <script>
+    import { formatDate } from '$lib/utils/formattingDate.js';
     export let data;
 </script>
 
 <div class="prose">
     <h1>{data.meta.title}</h1>
-    <p class="text-gray-500">{data.meta.date}</p>
+    <p class="text-gray-500">{formatDate(data.meta.date)}</p>
     {#if data.content}
         <svelte:component this={data.content} />
     {:else}
