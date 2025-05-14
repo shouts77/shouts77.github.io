caf59ae4fa7aa34d0d1f5455d9fa58e7dfbd563e
shouts77 <shouts98@gmail.com>
2025-05-14 15:02:46 +0900

    add main page and first log

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,29 @@
-<div class="flex justify-left w-full px-4 pt-2.5 md:justify-left md:w-96 md:px-4">
-	<p class="font-yoo text-sm">SvelteKit + Tailwind CSS로 제작중인 페이지입니다.</p>
+<div class="flex flex-col items-left w-full px-4 pt-2.5 md:justify-left md:w-96 md:px-4">
+  <p class="font-yoo text-sm">SvelteKit으로 일상을 기록하는 공간을 만들었습니다. 늘 배우며 성장하기를 꿈꾸고 있습니다.</p>
 </div>
+
+<div class="flex flex-col items-center w-full px-4 pt-2.5 md:justify-left md:w-96 md:px-4">
+  <p class="font-yoo text-sm text-blue-900 mb-2">" We will find a way. We always have. - Interstellar "</p>
+	
+  <!-- 이미지 영역 -->
+  <figure class="w-full mb-2">
+	<div class="w-full mb-2 overflow-hidden rounded-lg shadow-md">
+		<img 
+		src="/mainpage/universe.png" 
+		alt="Universe" 
+		class="w-full h-auto object-cover hover:scale-105 transition-transform duration-300"
+		loading="lazy"
+		/>
+	</div>
+    <figcaption class="font-yoo text-xs text-gray-500 mt-1 text-right">
+      photo by Hristo Fidanov
+	  <br />
+	  font by 나눔글꼴
+    </figcaption>
+
+  </figure>
+  
+  
+
+</div>
+
