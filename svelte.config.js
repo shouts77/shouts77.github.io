fe4f5b06cf66a2dbebb529d51aea0b5bde0d913b
shouts77 <shouts98@gmail.com>
2025-06-05 08:12:55 +0900

    Configure for user site

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +12,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+    	adapter: adapter({
+			// 정적 파일이 생성될 디렉토리
+      	pages: 'build',
+      	assets: 'build',
+      	fallback: '404.html',
+      	precompress: false
+    	}),
+        // 사용자 사이트는 base 경로가 필요 없음
+        paths: {
+          base: '' // 사용자이름.github.io는 base 경로가 없음
+        }
 	},
 	preprocess: [
         mdsvex({
