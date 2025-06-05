ce83fc570a2bda645cf44f6564e1e6f60a817dce
shouts77 <shouts98@gmail.com>
2025-06-05 13:23:09 +0900

    update sitemap and rss feed for github page

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -2,53 +2,46 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 import matter from 'gray-matter';
+import { getPosts } from '$lib/posts';
 
 export async function GET({ url }) {
-  const siteUrl = url.origin; // 또는 실제 도메인으로 하드코딩
-  const postsDirectory = path.resolve('src/posts');
-  const filenames = fs.readdirSync(postsDirectory);
-
-  // 모든 블로그 포스트 정보 가져오기
-  const posts = filenames
-    .filter((file) => file.endsWith('.md'))
-    .map((filename) => {
-      const filePath = path.join(postsDirectory, filename);
-      const fileContents = fs.readFileSync(filePath, 'utf-8');
-      const { data } = matter(fileContents);
-      
-      return {
-        slug: filename.replace('.md', ''),
-        date: data.date ? new Date(data.date).toISOString().split('T')[0] : new Date().toISOString().split('T')[0]
-      };
-    });
-
-  // sitemap XML 생성
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${siteUrl}/</loc>
-    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>${siteUrl}/blog</loc>
-    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  ${posts.map(post => `
-  <url>
-    <loc>${siteUrl}/blog/${post.slug}</loc>
-    <lastmod>${post.date}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>`).join('')}
-</urlset>`;
-
+  const posts = await getPosts();
+  
+  // GitHub Pages URL로 변경
+  const baseUrl = 'https://shouts77.github.io';
+  
+  const xml = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <!-- 메인 페이지 -->
+    <url>
+      <loc>${baseUrl}/</loc>
+      <changefreq>daily</changefreq>
+      <priority>1.0</priority>
+    </url>
+    
+    <!-- 블로그 목록 페이지 -->
+    <url>
+      <loc>${baseUrl}/blog</loc>
+      <changefreq>daily</changefreq>
+      <priority>0.8</priority>
+    </url>
+    
+    <!-- 각 블로그 포스트 -->
+    ${posts.map(post => `
+    <url>
+      <loc>${baseUrl}/blog/${post.slug}</loc>
+      <lastmod>${new Date(post.date).toISOString().split('T')[0]}</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>0.7</priority>
+    </url>
+    `).join('')}
+  </urlset>`.trim();
+  
   return new Response(xml, {
     headers: {
-      'Content-Type': 'application/xml'
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'max-age=0, s-maxage=3600'
     }
   });
 }
